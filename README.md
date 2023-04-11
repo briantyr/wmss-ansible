a268db3be2c4c970e35718ff8d0c8208b5f397fd
Brian <btaylormd@gmail.com>
2023-04-11 00:56:28 -0400

    initial commit, contains 3 small Ubuntu Ansible roles for updating the servers.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # wmss-ansible
+
+
 Small ansible playbooks for updating &amp; upgrading Ubuntu VMs from 16.x to 22.x (latest version 22.04.2)
+
+
+## Create virtualenv
+
+### setup_ansible.sh
+
+First step is to setup the virtual environment, and install the proper pip packages.  We can run `setup_ansible.sh` to create the virtualenv.
+
+In this particular use case, we are running this code to upgrade from various versions of Ubuntu 16.x and 18.x to the latest (22.x.x).  
+
+Execute `setup_ansible.sh`, create the virtualenv.
+
+
+### Run playbook
+
+Next steps would be to run the `playbook_main.yml` file.  Adjust the `inventory/wmss/hosts` file as needed, per your environment.
+
+`ansible-playbok -i inventory/wmss/hosts playbook_main.yml -vv`
---
+++ setup_ansible.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+#
+# Ansible 7.4 environment setup
+# Python 3.11.2 testing
+#
+# SOURCE THIS FILE, DO NOT RUN IT
+# source setup_ansible.sh
+
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "Hey, you should source this script, not execute it!"
+    exit 1
+fi
+
+echo ""
+echo "#################################"
+echo "# Setting up Ansible Basesystem #"
+echo "#################################"
+echo ""
+
+echo "# This virtualenv was created with Python 3.11.2 (homebrew). #"
+
+# If you need to install these packages with pip, make sure
+# you are within a virtualenv.
+
+# Create with 'python3 -m venv ~/.virtualenv/ansible' and then source
+# into the environment and use pip to install the packages
+# with 'pip3 install -r requirements.txt'.
+
+
+
+
+###############################################################`
+# YOU NEED TO SOURCE THIS FILE TO ENTER THE VIRTUAL ENV
+###############################################################`
+
+export VENV_NAME="ansible-test-tmp"
+function install_pip() {
+	mkdir -p "$HOME/.virtualenvs"
+	python3 -m venv "$HOME/.virtualenvs/${VENV_NAME}"
+	if [[ $? -ne 0 ]]; then
+		echo "# Failed to create the virtualenv.  Exiting.. #"
+		exit 1
+	fi
+	source "$HOME/.virtualenvs/${VENV_NAME}/bin/activate"
+	if [[ $? -ne 0 ]]; then
+		echo "# Failed to source into the virtualenv, exiting. #"
+		exit 1
+	fi
+	pip3 install -U -r requirements.txt
+}
+
+## If needed, run the install function to setup the virtualenv.
+install_pip
+
+source "$HOME/.virtualenvs/${VENV_NAME}/bin/activate"
+
+# After the virtualenv is up, we can run the playbook 'playbook_main.yml'.
